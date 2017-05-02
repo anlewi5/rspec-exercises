8a25179cbf3f6a610a074c22ca920a42b8a07db6
Matt Lewis <matt@mplewis.com>
2017-05-01 20:09:55 -0600

    exercise: puppy

---
+++ puppy/lib/puppy.rb
@@ -0,0 +1,21 @@
+class Puppy
+  def initialize
+    # Your code here
+  end
+
+  def pet
+    # Your code here
+  end
+
+  def rub_belly
+    # Your code here
+  end
+
+  def spray
+    # Your code here
+  end
+
+  def speak
+    # Your code here
+  end
+end
