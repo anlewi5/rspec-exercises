51fef9de05e2368dc782085ab94a2edbc0c4b13a
Anna Lewis <anna.noel.lewis@gmail.com>
2017-05-14 18:28:03 -0600

    Made calc.rb pass tests

--- calculator/lib/calc.rb
+++ calculator/lib/calc.rb
@@ -1,31 +1,61 @@
 module Calc
+  #idiom: ""
   class << self
     def add(*args)
-      # your code here
+      sum = 0
+      args.each {
+        |number|
+        sum += number
+      }
+      return sum
     end
 
     def sub(a, b)
-      # your code here
+      a - b
     end
 
     def mul(*args)
-      # your code here
+      prod = 1
+      args.each {
+        |number|
+        prod *= number
+      }
+      return prod
     end
 
     def div(a, b)
-      # your code here
+      #note: the .to_f makes sure that floating point answers are returned
+      a.to_f / b
     end
 
     def factors(n)
-      # your code here
+      counter = n
+      factors = [n]
+      return "not a positive integer" if n <= 1
+      loop {
+        factors.push(counter - 1) if n % (counter - 1) === 0
+        counter -= 1
+        break if counter == 1
+      }
+      return factors.reverse
     end
 
     def prime(n)
-      # your code here
+      y = Calc.factors(n)
+      if y.length === 2
+        return true
+      else
+        return false
+      end
     end
 
     def primes_in_range(first, last)
-      # your code here
+      prime_array = []
+      (first..last).each {
+        |num|
+        prime_array.push(num) if Calc.prime(num) === true
+      }
+      return prime_array
     end
   end
 end
