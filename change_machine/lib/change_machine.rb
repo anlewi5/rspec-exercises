e853942cff12f265fdb8ad8dfaaa4cb14e8d3b21
Anna Lewis <anna.noel.lewis@gmail.com>
2017-05-14 18:15:48 -0600

    Made change_machine.rb pass tests

--- change_machine/lib/change_machine.rb
+++ change_machine/lib/change_machine.rb
@@ -1,22 +1,40 @@
-# You can modify this class however you like to make it pass the test suite.
 class ChangeMachine
-  def initialize
-    # Your code here
-  end
-
   def cost(cents)
-    # Your code here
+    @cost = cents
   end
 
   def paid(cents)
-    # Your code here
+    @paid = cents
+  end
+
+  def change_maker(difference)
+    #takes difference and returns quarters, dimes, nickels and pennies
+    while difference != 0
+      if difference >= 25
+        quarter
+        difference -= 25
+      elsif difference >= 10
+        dime
+        difference -= 10
+      elsif difference >= 5
+        nickel
+        difference -= 5
+      elsif difference >= 1
+        penny
+        difference -= 1
+      end
+    end
   end
 
   def dispense_change
-    # Your code here
+    #sets up for change_maker method (makes sure difference btwn 0 and 99)
+    difference = @paid - @cost
+    return if difference <= 0
+    difference = difference % 100
+    change_maker(difference)
   end
 
-  # You don't have to modify anything under this line
+  # Money methods
 
   def quarter
     puts 'Dispensed quarter'
